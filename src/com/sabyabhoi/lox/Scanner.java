3eb9f912e5f137f63a4186632824ea6c90bff431
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2025-07-11 22:13:43 +0530

    Add keyword map

--- src/com/sabyabhoi/lox/Scanner.java
+++ src/com/sabyabhoi/lox/Scanner.java
@@ -1,11 +1,35 @@
 package com.sabyabhoi.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.sabyabhoi.lox.TokenType.*;
 
 public class Scanner {
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
+
     private final String source;
     private final List<Token> tokens = new ArrayList<Token>();
 
@@ -73,11 +97,30 @@ public class Scanner {
             default:
                 if(isDigit(c)) {
                     number();
-                } else {
+                } else if(isAlpha(c)) {
+                    identifier();
+                }
+                else {
                     Lox.error(line, "Unrecognized character '" + (char) c + "'");
                 }
                 break;
         }
+    }
+
+    private void identifier() {
+        while(isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.getOrDefault(text, IDENTIFIER);
+        addToken(type);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 
     private boolean isDigit(char c) {
