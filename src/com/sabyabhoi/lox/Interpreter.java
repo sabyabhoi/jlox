f5d23a85fa90d545bc108b7411429505ffb9e4b2
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2025-11-15 09:29:12 +0530

    Add assignment Expr
    Assignment is different from declaration. For assignment, the variable
    must already be present in the environment. Whereas in case of
    declaration, we're declaring a new variable entirely.

--- src/com/sabyabhoi/lox/Interpreter.java
+++ src/com/sabyabhoi/lox/Interpreter.java
@@ -95,6 +95,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
+
+    @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
         evaluate(stmt.expression);
         return null;
